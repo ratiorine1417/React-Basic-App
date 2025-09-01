29c94fa31f381fcd55dbef6056f0408312e1b6ac
ratiorine1417 <rataventurine@gmail.com>
2025-09-01 14:49:23 +0900

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import MyComponent from './components/MyComponent';
+import MyComponentFunc from './components/MyComponentFunc';
 
 const App = () => {
   return (
     <>
-      
+     <MyComponent name="ReactJS" age={100} />
+     <hr/>
+     <MyComponentFunc name="함수형">
+      <p>함수형 컴포넌트의 하위 엘리먼트</p>
+     </MyComponentFunc>
     </>
   );
 };
